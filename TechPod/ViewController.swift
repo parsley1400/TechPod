3f6d07e023f97c1b98981b90cd7ce80c6ca22df9
parsley1400 <topthecap@gmail.com>
2020-09-10 21:17:44 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by risako takeya on 2020/09/10.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
